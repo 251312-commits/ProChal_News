885d57475c9b480af885ca621d5ee46d2f3705d8
251312-commits <251312@sangmyung-agh.sen.hs.kr>
2026-09-19 16:08:06 +0900

    news.py 업데이트

--- news.py
+++ news.py
@@ -411,7 +411,7 @@ def show_main_page():
     img_urls = [
         "https://github.com/user-attachments/assets/331b7b2e-c4f5-4087-9f6b-9e672afb9568", 
         "https://github.com/user-attachments/assets/24bc9c42-e6c0-4f6f-a3a1-e1cce8fd66f3", 
-        "https://picsum.photos/id/30/800/110", 
+        "https://github.com/user-attachments/assets/caf0511f-54f9-4d1e-8d6d-318e6886f2b4", 
         "https://picsum.photos/id/40/800/110", 
         "https://picsum.photos/id/50/800/110", 
         "https://picsum.photos/id/60/800/110", 
