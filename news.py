28e94501b5e9491b0a0be67cb6ca8e0552cdc020
251312-commits <251312@sangmyung-agh.sen.hs.kr>
2026-09-26 23:17:46 +0900

    news.py 업데이트

--- news.py
+++ news.py
@@ -1,14 +1,22 @@
 import streamlit as st
-import gspread
-from google.oauth2.service_account import Credentials
-from st_clickable_images import clickable_images
-from newspaper import Article
-import re
-from urllib.parse import urlparse
-from bs4 import BeautifulSoup
-import torch
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
-from streamlit_autorefresh import st_autorefresh
-import time
-import streamlit.components.v1 as components
-import random
+from views.login_view import show_login_page
+from views.main_view import show_main_page
+from modules.ui_components import play_bgm, stop_bgm, inject_casino_theme
+
+# 세션 상태 초기화
+if 'page' not in st.session_state:
+    st.session_state.page = 'login'
+if 'login_step' not in st.session_state:
+    st.session_state.login_step = 1
+
+# 글로벌 테마 적용
+inject_casino_theme()
+
+# 페이지 라우팅
+if st.session_state.page == 'login':
+    stop_bgm()
+    show_login_page()
+
+elif st.session_state.page == 'main':
+    play_bgm()
+    show_main_page()
